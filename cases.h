5b4a717a0db16328d2fecf455a4c305b193ce492
vichauvi <117533728+vichauvi@users.noreply.github.com>
2023-07-29 00:26:21 -0400

    Add files via upload

---
+++ cases.h
@@ -0,0 +1,35 @@
+#ifndef CASES_H
+#define CASES_H
+extern struct term CitiesSorted[93827];
+extern struct term TorCases[179];
+extern struct term CockCases[5];
+extern struct term noCityWithThisNameCases[1];
+extern struct term BruCases[103];
+extern struct term PenCases[97];
+extern struct term DickCases[11];
+extern struct term LosCases[106];
+extern struct term CamCases[309];
+extern struct term TabonCases[4];
+extern struct term LigCases[23];
+extern struct term BallCases[52];
+extern struct term HiCases[341];
+extern struct term HellCases[11];
+extern struct term WikiSorted[10000];
+extern struct term susCases[11];
+extern struct term impoCases[10];
+extern struct term apCases[50];
+extern struct term deCases[227];
+extern struct term beCases[108];
+extern struct term treCases[24];
+extern struct term thCases[119];
+extern struct term canCases[19];
+extern struct term dickCases[2];
+extern struct term ballsCases[1];
+extern struct term neverCases[2];
+extern struct term goingCases[1];
+extern struct term toCases[59];
+extern struct term giveCases[3];
+extern struct term youCases[13];
+extern struct term upCases[10];
+extern struct term momCases[4];
+#endif
